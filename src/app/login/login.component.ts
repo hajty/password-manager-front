11c35d250f66709dbe22468c7db4830f57c72cdf
Bartosz Miłosierny <milosierny.bartosz@gmail.com>
2021-02-01 18:26:01 +0100

    small changes to log in form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,11 +9,10 @@ import { User } from '../users/shared/user.model';
 export class LoginComponent {
   constructor() { }
   model = { email: '', password: '' };
-  user = null;
 
   public onSubmit(): void {
     const email = this.model.email.trim();
     const password = this.model.password.trim();
-    this.user = new User(email, password);
+    const user = new User(email, password);
   }
 }
