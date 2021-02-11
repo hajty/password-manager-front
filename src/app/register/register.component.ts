cbacddc2782388dcd22f1dd21261757af63fc951
Bartosz Miłosierny <milosierny.bartosz@gmail.com>
2021-02-11 18:49:22 +0100

    registration complete

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { interval, timer } from 'rxjs';
 
 import { User } from '../users/shared/user.model';
 import { LoginService } from 'src/app/_services/login.service';
@@ -9,17 +11,32 @@ import { LoginService } from 'src/app/_services/login.service';
   styleUrls: ['./register.component.css'],
   providers: [ LoginService ]
 })
+
 export class RegisterComponent {
-  constructor(private loginService: LoginService) { }
-  model = { email: '', password: '', confirmPassword: '' };
+  model = { oldEmail: '', email: '', password: '', confirmPassword: '' };
   registrationResult = null;
+  timeToRedirect = 5;
+  constructor(private router: Router, private loginService: LoginService) {}
+  private clearEmail(): void {
+    this.model.oldEmail = this.model.email;
+    this.model.email = '';
+  }
   onSubmit(): void {
     const email = this.model.email.trim();
     const password = this.model.password.trim();
     const user = new User(email, password);
-    this.loginService.register(user).subscribe((addedUser: User) => {
-      if (addedUser != null) { console.log(`Added user id: ${addedUser.id}`); }
-      else { console.log(`nope`); }
+    this.loginService.register(user).subscribe(resp => {
+      if (resp.status === 201) {
+        this.registrationResult = 'success';
+        timer(5000).subscribe(() => { this.router.navigate(['/login']).then(); });
+        interval(1000).subscribe(() => { this.timeToRedirect--; });
+      }
+    }, error => {
+      if (error.status === 409 && error.error === 'User already exists') {
+        this.registrationResult = 'user exists';
+        this.clearEmail();
+      }
+      if (error.status === 400 && error.error === 'Wrong body format') { this.registrationResult = 'wrong body format'; }
     });
   }
 }
