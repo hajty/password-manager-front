6eb5e60f3172752467b705cbcf38e2bc76f067d3
Bartosz Miłosierny <milosierny.bartosz@gmail.com>
2021-02-11 18:49:10 +0100

    register function done

--- src/app/_services/login.service.ts
+++ src/app/_services/login.service.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { User } from 'src/app/users/shared/user.model';
 import { URL_REGISTER } from 'src/configs/app.config';
-import { catchError, tap } from 'rxjs/operators';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,25 +11,13 @@ export class LoginService {
   constructor(
       private http: HttpClient
   ) {}
-
-  private handleError<T>(result?: T): (error: any) => Observable<T> {
-      return (error: any): Observable<T> => {
-          console.error('error');
-
-          return of(result as T);
-      };
-  }
-
-  register(user: User): Observable<User> {
+  register(user: User): Observable<HttpResponse<any>> {
       const userToAdd = {
               user: {
                       email: user.email,
                       password: user.password
                   }
           };
-      return this.http.post<User>(URL_REGISTER, userToAdd, { observe: 'body' }).pipe(
-          tap((addedUser: User) => console.log(`Registered. Id: ${addedUser.id}`)),
-          catchError(this.handleError<User>())
-      );
+      return this.http.post<User>(URL_REGISTER, userToAdd, { observe: 'response' });
   }
 }
