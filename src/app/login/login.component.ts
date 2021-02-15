ab8d2b604359dd346363c45528e7c77cf33d2193
Bartosz Miłosierny <milosierny.bartosz@gmail.com>
2021-02-15 21:04:55 +0100

    login with redirect to password component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,37 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { User } from '../users/shared/user.model';
+import { LoginService } from 'src/app/_services/login.service';
+import { Token } from 'src/app/users/shared/token.model';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  constructor() { }
-  model = { email: '', password: '' };
 
-  public onSubmit(): void {
+export class LoginComponent {
+  model = { email: '', password: '' };
+  loginResult = null;
+  constructor(private router: Router, private loginService: LoginService) {}
+  onSubmit(): void {
     const email = this.model.email.trim();
     const password = this.model.password.trim();
     const user = new User(email, password);
+    this.loginService.login(user).subscribe(resp => {
+      if (resp.status === 200) {
+        const token = new Token(resp.body.accessToken, resp.body.expiresIn);
+        this.loginService.setToken(token);
+        this.router.navigate(['/passwords']).then();
+      }
+    }, error => {
+      if (error.status === 400) {
+        console.log(400);
+      }
+      if (error.status === 401) {
+        console.log(401);
+      }
+    });
   }
 }
