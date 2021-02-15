a0cf76b2f00162e98549693fe2495318fe3cb800
Bartosz Miłosierny <milosierny.bartosz@gmail.com>
2021-02-15 21:05:20 +0100

    added token storage and login function

--- src/app/_services/login.service.ts
+++ src/app/_services/login.service.ts
@@ -1,23 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { User } from 'src/app/users/shared/user.model';
-import { URL_REGISTER } from 'src/configs/app.config';
+import { URL_LOGIN, URL_REGISTER } from 'src/configs/app.config';
 import { Observable } from 'rxjs';
+import { Token } from 'src/app/users/shared/token.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private token: Token;
+  setToken(token: Token): void {
+      this.token = token;
+  }
+  getToken(): Token {
+      return this.token;
+  }
   constructor(
       private http: HttpClient
   ) {}
   register(user: User): Observable<HttpResponse<any>> {
       const userToAdd = {
-              user: {
-                      email: user.email,
-                      password: user.password
-                  }
-          };
+          user: {
+              email: user.email,
+              password: user.password
+          }
+      };
       return this.http.post<User>(URL_REGISTER, userToAdd, { observe: 'response' });
+  }
+  login(user: User): Observable<HttpResponse<any>> {
+      const userToLogin = {
+          user: {
+              email: user.email,
+              password: user.password
+          }
+      };
+      return this.http.post<Token>(URL_LOGIN, userToLogin, { observe: 'response' });
   }
 }
