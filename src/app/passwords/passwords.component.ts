e9a734f6e3c6b6482680c11dc9cd846a0d92738c
Bartosz Miłosierny <milosierny.bartosz@gmail.com>
2021-02-17 23:41:21 +0100

    implemented getting passwords from api

--- src/app/passwords/passwords.component.ts
+++ src/app/passwords/passwords.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, destroyPlatform, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/_services/login.service';
+import { PasswordsService } from 'src/app/_services/passwords.service';
+import { Router } from '@angular/router';
+import { Password } from 'src/app/users/shared/password.model';
 
 @Component({
   selector: 'app-passwords',
   templateUrl: './passwords.component.html',
   styleUrls: ['./passwords.component.css']
 })
-export class PasswordsComponent {
+export class PasswordsComponent implements OnInit {
   token = this.loginService.getToken();
-  passwords = [];
-  constructor(private loginService: LoginService) {}
+  passwords: Password[] = [];
+  constructor(
+      private loginService: LoginService,
+      private passwordsService: PasswordsService,
+      private route: Router) {}
+
+  ngOnInit(): void {
+    if (this.token != null) {
+      this.passwordsService.getPasswords(this.token).subscribe(
+          data => {
+            for (const password of data) {
+              this.passwords.push(password);
+            }
+          }
+      );
+    }
+    else {
+      this.route.navigate(['/login']).then();
+    }
+  }
 }
