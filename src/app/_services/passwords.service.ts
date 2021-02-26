c1e3212182f0c4d7b58ccd8d60df50aa628a6052
Bartosz Miłosierny <milosierny.bartosz@gmail.com>
2021-02-26 17:47:15 +0100

    added handleerror

--- src/app/_services/passwords.service.ts
+++ src/app/_services/passwords.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Token } from 'src/app/users/shared/token.model';
-import { Observable } from 'rxjs';
-import { Password } from 'src/app/users/shared/password.model';
+import { Token } from 'src/app/_models/shared/token.model';
+import { Observable, of } from 'rxjs';
+import { Password } from 'src/app/_models/shared/password.model';
 import { URL_PASSWORDS } from 'src/configs/app.config';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +13,17 @@ export class PasswordsService {
   constructor(
       private http: HttpClient
   ) {}
+  private handleError<T>(operation = 'operation', result?: T): (error: any) => Observable<T> {
+      return (error: any): Observable<T> => {
+          console.error(`${operation} error: ${error.message}`);
+          return of(result as T);
+      };
+  }
   getPasswords(token: Token): Observable<Password[]> {
-    return this.http.get<Password[]>(
-        URL_PASSWORDS, { headers: {
+    return this.http.get<Password[]>(URL_PASSWORDS, { headers: {
           authorization: token.accessToken
-          },
-          observe: 'body' });
+          }, observe: 'body' }).pipe(
+              catchError(this.handleError<Password[]>('getPasswords', []))
+    );
   }
 }
