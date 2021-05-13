6301a36f15ae0417fffec68fce81ed3a64f18d62
Bartosz Miłosierny <milosierny.bartosz@gmail.com>
2021-05-13 20:42:26 +0200

    added _isEdited property and changed _id, _isShow to optional values

--- src/app/_models/shared/password.model.ts
+++ src/app/_models/shared/password.model.ts
@@ -1,6 +1,7 @@
 export interface IPassword {
-    _id: number;
-    _isShown: boolean;
+    _id?: number;
+    _isShown?: boolean;
+    _isEdited?: boolean;
     service: string;
     username: string;
     password: string;
