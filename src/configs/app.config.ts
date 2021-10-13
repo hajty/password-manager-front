0d3301ef6975f20792114b4bea410d47ad843f8b
Bartosz Miłosierny <milosierny.bartosz@gmail.com>
2021-10-13 16:43:50 +0200

    added url for refresh token

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -5,3 +5,4 @@ export const URL_REGISTER = `${DOMAIN}/register`;
 export const URL_LOGIN = `${DOMAIN}/login`;
 export const URL_PASSWORDS = `${API_DOMAIN}/passwords`;
 export const URL_PASSWORD = `${API_DOMAIN}/password`;
+export const URL_TOKEN = `${DOMAIN}/token`;
