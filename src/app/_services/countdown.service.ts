20e27ea64acf38b2c9a9685c4ed99e3fe93bd0ff
Bartosz Miłosierny <milosierny.bartosz@gmail.com>
2021-10-13 16:44:53 +0200

    added filter option to select countdown output string

--- src/app/_services/countdown.service.ts
+++ src/app/_services/countdown.service.ts
@@ -7,7 +7,7 @@ import { interval, Observable, Subscription } from 'rxjs';
 export class CountdownService {
   constructor() { }
 
-  countdown(time: number): Observable<string> {
+  countdown(time: number, filter: string): Observable<string> {
     return new Observable<string>((o) => {
       let subTimer: Subscription;
       const oTimer = interval(1000);
@@ -19,7 +19,22 @@ export class CountdownService {
         const minutesToReturn = (minutes >= 10) ? minutes : '0' + minutes;
         const seconds = timeToCountdown - minutes * 60;
         const secondsToReturn = (seconds >= 10) ? seconds : '0' + seconds;
-        o.next(`${minutesToReturn}:${secondsToReturn}`);
+
+        switch (filter) {
+          case 'mm:ss': {
+            o.next(`${minutesToReturn}:${secondsToReturn}`);
+            break;
+          }
+          case 'ss': {
+            o.next(`${secondsToReturn}`);
+            break;
+          }
+          case 's': {
+            o.next(`${secondsToReturn.toString().slice(1)}`);
+            break;
+          }
+        }
+
         if (timeToCountdown <= 0) {
           subTimer.unsubscribe();
           o.complete();
