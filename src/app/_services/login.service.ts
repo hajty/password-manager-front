74c4771092e05312373c7226938d0041050722de
Bartosz Miłosierny <milosierny.bartosz@gmail.com>
2021-10-13 16:45:49 +0200

    added refresh token function

--- src/app/_services/login.service.ts
+++ src/app/_services/login.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { User } from 'src/app/_models/shared/user.model';
-import { URL_LOGIN, URL_REGISTER } from 'src/configs/app.config';
+import { URL_LOGIN, URL_REGISTER, URL_TOKEN } from 'src/configs/app.config';
 import { Observable } from 'rxjs';
 import { Token } from 'src/app/_models/shared/token.model';
 
@@ -9,17 +9,21 @@ import { Token } from 'src/app/_models/shared/token.model';
   providedIn: 'root'
 })
 export class LoginService {
-  private token: Token = null;
-  setToken(token: Token): void {
+    private token: Token = null;
+
+    setToken(token: Token): void {
       this.token = token;
-  }
-  getToken(): Token {
+    }
+
+    getToken(): Token {
       return this.token;
-  }
-  constructor(
+    }
+
+    constructor(
       private http: HttpClient
-  ) {}
-  register(user: User): Observable<HttpResponse<any>> {
+    ) {}
+
+    register(user: User): Observable<HttpResponse<any>> {
       const userToAdd = {
           user: {
               email: user.email,
@@ -27,8 +31,9 @@ export class LoginService {
           }
       };
       return this.http.post<User>(URL_REGISTER, userToAdd, { observe: 'response' });
-  }
-  login(user: User): Observable<HttpResponse<any>> {
+    }
+
+    login(user: User): Observable<HttpResponse<any>> {
       const userToLogin = {
           user: {
               email: user.email,
@@ -36,5 +41,12 @@ export class LoginService {
           }
       };
       return this.http.post<Token>(URL_LOGIN, userToLogin, { observe: 'response' });
-  }
+    }
+
+    refreshToken(token: Token): Observable<Token> {
+        return this.http.get<Token>(URL_TOKEN, {
+            headers: { authorization: token.refreshToken },
+            observe: 'body'
+        });
+    }
 }
