87a8ce6715a79ef4b9a7f992ef5f19d146783513
Bartosz Miłosierny <milosierny.bartosz@gmail.com>
2021-10-19 22:25:29 +0200

    made separate components for counter, adding and edit passwords. Made StoragePassword Component to enter decrypt password.

--- src/app/passwords/passwords.component.ts
+++ src/app/passwords/passwords.component.ts
@@ -1,150 +1,141 @@
-import { AfterViewChecked, Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit, AfterViewInit, AfterViewChecked } from '@angular/core';
 import { LoginService } from 'src/app/_services/login.service';
 import { PasswordsService } from 'src/app/_services/passwords.service';
 import { Router } from '@angular/router';
 import { IPassword } from 'src/app/_models/shared/password.model';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { HttpResponse } from '@angular/common/http';
-import { CountdownService } from 'src/app/_services/countdown.service';
-import { Subscription } from 'rxjs';
+import { StoragePasswordComponent } from 'src/app/storage-password/storage-password.component';
+import { EditPasswordComponent } from 'src/app/edit-password/edit-password.component';
+import { CounterComponent } from 'src/app/counter/counter.component';
+import { ToastService } from 'src/app/_services/toast.service';
 
 @Component({
-  selector: 'app-passwords',
-  templateUrl: './passwords.component.html',
-  styleUrls: ['./passwords.component.css']
+    selector: 'app-passwords',
+    templateUrl: './passwords.component.html',
+    styleUrls: ['./passwords.component.css'],
 })
-export class PasswordsComponent implements OnInit, AfterViewChecked {
-    private counter: Subscription;
-
+export class PasswordsComponent implements OnInit, AfterViewInit, AfterViewChecked {
+    @ViewChild(CounterComponent) counterComponent;
+    @ViewChild(StoragePasswordComponent) storagePasswordComponent;
+    @ViewChild(EditPasswordComponent) editPasswordComponent;
     @ViewChild('passwordModal') passwordModal: HTMLElement;
-    modalType = '';
-    timerText = '';
 
     token = this.loginService.getToken();
     passwords: IPassword[] = [];
     fetched = false;
-    editPassword: IPassword = {
-        service: '',
-        username: '',
-        password: ''
-    };
     constructor(
         private modalService: NgbModal,
         private loginService: LoginService,
         private passwordsService: PasswordsService,
-        private countDownService: CountdownService,
-        private route: Router) {}
-
-    private clearPassword(): void {
-        this.editPassword = {
-          service: '',
-          username: '',
-          password: ''
-        };
-    }
-
-    private refreshToken(): void {
-        this.loginService.refreshToken(this.token).subscribe(token => this.token = token);
-        this.setTimer();
-    }
-
-    private setTimer(): void {
-        if (this.counter != null) { this.counter.unsubscribe(); }
-        this.counter = this.countDownService.countdown(this.token.expiresIn, 'mm:ss').subscribe(
-            x => this.timerText = x,
-            () => {},
-            () => {
-                this.token = null;
-            });
-    }
+        private toastService: ToastService,
+        private route: Router
+    ) {}
 
     ngOnInit(): void {
         this.passwords = [];
         this.fetched = false;
+
         if (this.token != null) {
-            this.passwordsService.getPasswords(this.token).subscribe(
-              data => {
-                    for (const password of data) {
-                        this.passwords.push(password);
-                    }
-                    this.fetched = true;
-                    this.setTimer();
-              }
-            );
+            this.passwordsService.getPasswords(this.token).subscribe((data) => {
+                for (const password of data) {
+                    this.passwords.push(password);
+                }
+                // this.fetched = true;
+                this.counterComponent.setTimer();
+            });
         }
     }
 
-    ngAfterViewChecked(): void {
-        if (this.token == null) { this.route.navigate(['/logout']).then(); }
-    }
+    ngAfterViewInit(): void {}
 
-    onClickRefresh(): void {
-        this.refreshToken();
+    ngAfterViewChecked(): void {
+        if (this.token == null) {
+            this.route.navigate(['/logout']).then();
+        }
     }
 
     onClickLogout(): void {
         this.token = null;
+        this.toastService.showRed({
+            header: 'Logged out',
+            body: 'You have logged out!',
+        });
     }
 
     onClickAdd(): void {
-        this.modalType = 'add';
-        this.modalService.open(this.passwordModal, { centered: true })
-            .result.then((result) => {
-              if (result === 'save') {
-                this.passwordsService.postPassword(this.token, this.editPassword).subscribe(
-                    response => {
-                      if ((response as HttpResponse<any>).status === 201) {
-                        this.clearPassword();
-                        this.refreshToken();
-                        this.ngOnInit();
-                        alert('Successfully added new password!');
-                      }
+        this.editPasswordComponent.modalType = 'add';
+        this.modalService
+            .open(this.editPasswordComponent.editModal, { centered: true })
+            .result.then(
+                (result) => {
+                    if (result === 'save') {
+                        this.passwordsService
+                            .postPassword(this.token, this.editPasswordComponent.editPassword)
+                            .subscribe((response) => {
+                                if ((response as HttpResponse<any>).status === 201) {
+                                    this.editPasswordComponent.clearPassword();
+                                    this.counterComponent.refreshToken();
+                                    this.ngOnInit();
+                                    this.toastService.showGreen({
+                                        header: 'Added new Password',
+                                        body: 'Successfully added new Password!',
+                                    });
+                                }
+                            });
                     }
-                );
-              }
-            },
-            () => {
-              this.clearPassword();
-            });
+                },
+                () => {
+                    this.editPasswordComponent.clearPassword();
+                }
+            );
     }
 
     onClickEdit(password: IPassword): void {
-        this.modalType = 'edit';
-        this.editPassword._id = password._id;
-        this.editPassword.service = password.service;
-        this.editPassword.username = password.username;
-        this.editPassword.password = password.password;
+        this.editPasswordComponent.modalType = 'edit';
+        this.editPasswordComponent.editPassword._id = password._id;
+        this.editPasswordComponent.editPassword.service = password.service;
+        this.editPasswordComponent.editPassword.username = password.username;
+        this.editPasswordComponent.editPassword.password = password.password;
 
-        this.modalService.open(this.passwordModal, { centered: true })
-            .result.then((result) => {
-              if (result === 'save') {
-                this.passwordsService.updatePassword(this.token, this.editPassword).subscribe(
-                    response => {
-                      if ((response as HttpResponse<any>).status === 204) {
-                        this.clearPassword();
-                        this.refreshToken();
-                        this.ngOnInit();
-                        alert('Successfully updated new password!');
-                      }
+        this.modalService
+            .open(this.editPasswordComponent.editModal, { centered: true })
+            .result.then(
+                (result) => {
+                    if (result === 'save') {
+                        this.passwordsService
+                            .updatePassword(this.token, this.editPasswordComponent.editPassword)
+                            .subscribe((response) => {
+                                if ((response as HttpResponse<any>).status === 204) {
+                                    this.editPasswordComponent.clearPassword();
+                                    this.counterComponent.refreshToken();
+                                    this.ngOnInit();
+                                    this.toastService.showYellow({
+                                        header: 'Updated new Password',
+                                        body: 'Successfully updated new Password!',
+                                    });
+                                }
+                            });
+                        this.editPasswordComponent.clearPassword();
                     }
-                );
-                this.clearPassword();
-              }
-            },
-            () => {
-              this.clearPassword();
-            });
+                },
+                () => {
+                    this.editPasswordComponent.clearPassword();
+                }
+            );
     }
 
     onClickDelete(password: IPassword): void {
-        this.passwordsService.deletePassword(this.token, password).subscribe(
-            response => {
-              if ((response as HttpResponse<any>).status === 204) {
-                alert('Successfully deleted password!');
-                this.refreshToken();
+        this.passwordsService.deletePassword(this.token, password).subscribe((response) => {
+            if ((response as HttpResponse<any>).status === 204) {
+                this.toastService.showRed({
+                    header: 'Deleted Password',
+                    body: 'Successfully deleted Password!',
+                });
+                this.counterComponent.refreshToken();
                 this.ngOnInit();
-              }
-            });
+            }
+        });
     }
 
     onClickClipboard(text: string): void {
@@ -161,4 +152,3 @@ export class PasswordsComponent implements OnInit, AfterViewChecked {
         document.body.removeChild(textarea);
     }
 }
-
