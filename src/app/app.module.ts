fe73b83c8c059247a2636df21dba80caf4c6d212
Bartosz Miłosierny <milosierny.bartosz@gmail.com>
2021-10-19 22:22:57 +0200

    deleted HomeComponent, added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,34 +5,40 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PasswordsComponent } from './passwords/passwords.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { StoragePasswordComponent } from './storage-password/storage-password.component';
+import { EditPasswordComponent } from './edit-password/edit-password.component';
+import { CounterComponent } from './counter/counter.component';
+import { ToastsComponent } from './toasts/toasts.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    HomeComponent,
-    RegisterComponent,
-    PasswordsComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot([
-      { path: '', redirectTo: '/login', pathMatch: 'full' },
-      { path: 'logout', redirectTo: '/login', pathMatch: 'full'},
-      { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegisterComponent },
-      { path: 'passwords', component: PasswordsComponent },
-    ]),
-    NgbModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        RegisterComponent,
+        PasswordsComponent,
+        StoragePasswordComponent,
+        EditPasswordComponent,
+        CounterComponent,
+        ToastsComponent,
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        FormsModule,
+        RouterModule.forRoot([
+            { path: '', redirectTo: '/login', pathMatch: 'full' },
+            { path: 'logout', redirectTo: '/login', pathMatch: 'full' },
+            { path: 'login', component: LoginComponent },
+            { path: 'register', component: RegisterComponent },
+            { path: 'passwords', component: PasswordsComponent },
+        ]),
+        NgbModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
