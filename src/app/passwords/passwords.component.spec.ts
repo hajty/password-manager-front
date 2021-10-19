f753f7c1652c65c7c1ed7ddc622deded9bbe3f19
Bartosz Miłosierny <milosierny.bartosz@gmail.com>
2021-10-19 22:25:37 +0200

    added prettier

--- src/app/passwords/passwords.component.spec.ts
+++ src/app/passwords/passwords.component.spec.ts
@@ -3,23 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PasswordsComponent } from './passwords.component';
 
 describe('PasswordsComponent', () => {
-  let component: PasswordsComponent;
-  let fixture: ComponentFixture<PasswordsComponent>;
+    let component: PasswordsComponent;
+    let fixture: ComponentFixture<PasswordsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PasswordsComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [PasswordsComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PasswordsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PasswordsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
