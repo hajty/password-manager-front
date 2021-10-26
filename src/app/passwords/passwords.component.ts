fb7c7032877b7bd554c53e460e9622e65ea823e8
Bartosz Miłosierny <milosierny.bartosz@gmail.com>
2021-10-26 14:08:52 +0200

    added encrypt/decrypt functions for passwords + changed component behaviour after changing passwords data

--- src/app/passwords/passwords.component.ts
+++ src/app/passwords/passwords.component.ts
@@ -1,11 +1,10 @@
-import { Component, ViewChild, OnInit, AfterViewInit, AfterViewChecked } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { LoginService } from 'src/app/_services/login.service';
 import { PasswordsService } from 'src/app/_services/passwords.service';
 import { Router } from '@angular/router';
 import { IPassword } from 'src/app/_models/shared/password.model';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { HttpResponse } from '@angular/common/http';
-import { StoragePasswordComponent } from 'src/app/storage-password/storage-password.component';
 import { EditPasswordComponent } from 'src/app/edit-password/edit-password.component';
 import { CounterComponent } from 'src/app/counter/counter.component';
 import { ToastService } from 'src/app/_services/toast.service';
@@ -18,14 +17,16 @@ import { CryptoService } from 'src/app/_services/crypto.service';
 })
 export class PasswordsComponent implements OnInit, AfterViewInit, AfterViewChecked {
     @ViewChild(CounterComponent) counterComponent;
-    @ViewChild(StoragePasswordComponent) storagePasswordComponent;
     @ViewChild(EditPasswordComponent) editPasswordComponent;
     @ViewChild('passwordModal') passwordModal: HTMLElement;
 
     token = this.loginService.getToken();
+    encryptedPasswords: IPassword[] = [];
     passwords: IPassword[] = [];
     fetched = false;
     storagePasswordEntered = false;
+    newUser;
+
     constructor(
         private modalService: NgbModal,
         private loginService: LoginService,
@@ -35,15 +36,21 @@ export class PasswordsComponent implements OnInit, AfterViewInit, AfterViewCheck
         private cryptoService: CryptoService
     ) {}
 
+    async getPasswordEntered(): Promise<void> {
+        this.storagePasswordEntered = true;
+        for (const password of this.encryptedPasswords) {
+            this.passwords.push(await this.cryptoService.decryptPassword(password));
+        }
+    }
+
     ngOnInit(): void {
-        this.passwords = [];
+        this.encryptedPasswords = [];
         this.fetched = false;
 
         if (this.token != null) {
             this.passwordsService.getPasswords(this.token).subscribe((data) => {
-                for (const password of data) {
-                    this.passwords.push(password);
-                }
+                this.encryptedPasswords = data;
+                this.newUser = this.encryptedPasswords.length === 0;
                 this.fetched = true;
                 this.counterComponent.setTimer();
             });
@@ -70,28 +77,28 @@ export class PasswordsComponent implements OnInit, AfterViewInit, AfterViewCheck
         this.editPasswordComponent.modalType = 'add';
         this.modalService
             .open(this.editPasswordComponent.editModal, { centered: true })
-            .result.then(
-                (result) => {
-                    if (result === 'save') {
-                        this.passwordsService
-                            .postPassword(this.token, this.editPasswordComponent.editPassword)
-                            .subscribe((response) => {
-                                if ((response as HttpResponse<any>).status === 201) {
-                                    this.editPasswordComponent.clearPassword();
-                                    this.counterComponent.refreshToken();
-                                    this.ngOnInit();
-                                    this.toastService.showGreen({
-                                        header: 'Added new Password',
-                                        body: 'Successfully added new Password!',
-                                    });
-                                }
-                            });
-                    }
-                },
-                () => {
-                    this.editPasswordComponent.clearPassword();
+            .result.then(async (result) => {
+                if (result === 'save') {
+                    const editPassword = this.editPasswordComponent.editPassword;
+                    const encryptedPassword = await this.cryptoService.encryptPassword(
+                        editPassword
+                    );
+                    this.passwordsService
+                        .postPassword(this.token, encryptedPassword)
+                        .subscribe((response) => {
+                            if ((response as HttpResponse<IPassword>).status === 201) {
+                                editPassword._id = (response as HttpResponse<IPassword>).body._id;
+                                this.passwords.push(editPassword);
+                                this.editPasswordComponent.clearPassword();
+                                this.counterComponent.refreshToken();
+                                this.toastService.showGreen({
+                                    header: 'Added new Password',
+                                    body: 'Successfully added new Password!',
+                                });
+                            }
+                        });
                 }
-            );
+            });
     }
 
     onClickEdit(password: IPassword): void {
@@ -104,19 +111,27 @@ export class PasswordsComponent implements OnInit, AfterViewInit, AfterViewCheck
         this.modalService
             .open(this.editPasswordComponent.editModal, { centered: true })
             .result.then(
-                (result) => {
+                async (result) => {
                     if (result === 'save') {
+                        const editPassword = this.editPasswordComponent.editPassword;
+                        const encryptedPassword = await this.cryptoService.encryptPassword(
+                            editPassword
+                        );
                         this.passwordsService
-                            .updatePassword(this.token, this.editPasswordComponent.editPassword)
+                            .updatePassword(this.token, encryptedPassword)
                             .subscribe((response) => {
                                 if ((response as HttpResponse<any>).status === 204) {
-                                    this.editPasswordComponent.clearPassword();
                                     this.counterComponent.refreshToken();
-                                    this.ngOnInit();
                                     this.toastService.showYellow({
                                         header: 'Updated new Password',
                                         body: 'Successfully updated new Password!',
                                     });
+                                    this.passwords[
+                                        this.passwords.findIndex(
+                                            (item) => item._id === editPassword._id
+                                        )
+                                    ] = editPassword;
+                                    this.editPasswordComponent.clearPassword();
                                 }
                             });
                         this.editPasswordComponent.clearPassword();
@@ -131,12 +146,12 @@ export class PasswordsComponent implements OnInit, AfterViewInit, AfterViewCheck
     onClickDelete(password: IPassword): void {
         this.passwordsService.deletePassword(this.token, password).subscribe((response) => {
             if ((response as HttpResponse<any>).status === 204) {
+                this.passwords = this.passwords.filter((item) => item._id !== password._id);
+                this.counterComponent.refreshToken();
                 this.toastService.showRed({
                     header: 'Deleted Password',
                     body: 'Successfully deleted Password!',
                 });
-                this.counterComponent.refreshToken();
-                this.ngOnInit();
             }
         });
     }
