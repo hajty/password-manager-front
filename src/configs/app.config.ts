b6b70f66ce32c6e8d2bef6e908bdcf549f3c34bc
Bartosz Miłosierny <milosierny.bartosz@gmail.com>
2021-10-26 20:44:57 +0200

    changed domain for heroku

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,6 +1,6 @@
-const DOMAIN = 'http://localhost:3000';
-const API_DOMAIN = 'http://localhost:3000/api';
-export const APP_TITLE = 'hajty\'s Password Manager';
+const DOMAIN = 'https://password-manager-back.herokuapp.com/';
+const API_DOMAIN = 'https://password-manager-back.herokuapp.com/api';
+export const APP_TITLE = "hajty's Password Manager";
 export const URL_REGISTER = `${DOMAIN}/register`;
 export const URL_LOGIN = `${DOMAIN}/login`;
 export const URL_PASSWORDS = `${API_DOMAIN}/passwords`;
